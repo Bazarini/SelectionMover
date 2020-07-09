df1df26cadbea894f394784ad4c60b42608d0deb
Bazarini <4438853@gmail.com>
2020-07-09 21:50:57 +0300

    First Commit

---
+++ SelectionMover/Models/ArrayWorker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SelectionMover.Models
+{
+    public static class ArrayWorker
+    {
+        public static int[,] NewArray(uint frame_width, uint frame_height)
+        {
+            Random random = new Random();
+            int[,] array = new int[frame_height, frame_width];
+            for (uint i = 0; i < frame_height; i++)
+            {
+                for (uint j = 0; j < frame_width; j++)
+                {
+                    array[i,j] = random.Next(int.MinValue, int.MaxValue);
+                }
+
+            }
+
+            return array;
+        }
+
+        public static int[,] MoveSelection(int[,] array, uint x, uint y, uint width, uint height)
+        {
+            for (uint i = 0; i < height; i++)
+            {
+                for (uint j = 0; j < width; j++)
+                {
+                    array[i, j] = array[i + y, j + x];
+                }
+            }
+            return array;
+        }
+
+
+    }
+}
---
+++ SelectionMover/Program.cs
@@ -0,0 +1,80 @@
+﻿using SelectionMover.Models;
+using System;
+
+namespace SelectionMover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint frame_width, frame_height, pic_width, pic_height, pic_x, pic_y;
+
+            frame_width = GetVariable("frame_width");
+            frame_height = GetVariable("frame_height");
+
+            int[,] array = ArrayWorker.NewArray(frame_width, frame_height);
+            pic_x = GetVariable("pic_x");
+            pic_width = GetVariable("pic_width");
+            while (pic_x + pic_width > frame_width)
+            {
+                Console.WriteLine("selection width is out of frame borders. Please re-enter the variables");
+                pic_x = GetVariable("pic_x");
+                pic_width = GetVariable("pic_width");
+            }
+            pic_y = GetVariable("pic_y");
+            pic_height = GetVariable("pic_height");
+            while (pic_y + pic_height > frame_height)
+            {
+                Console.WriteLine("selection height is out of frame borders. Please re-enter the variables");
+                pic_y = GetVariable("pic_y");
+                pic_height = GetVariable("pic_height");
+            }
+
+            for (int i = 0; i < frame_height; i++)
+            {
+                for (int j = 0; j < frame_width; j++)
+                {
+                    if (i >= pic_y && i < pic_height + pic_y && j >= pic_x && j < pic_x + pic_width)
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            array = ArrayWorker.MoveSelection(array, pic_x, pic_y, pic_width, pic_height);
+            for (int i = 0; i < frame_height; i++)
+            {
+                for (int j = 0; j < frame_width; j++)
+                {
+                    if (i < pic_height && j < pic_width)
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+        static uint GetVariable(string varName)
+        {
+            uint variable;
+            string temp = "";
+            Console.Write($"{varName} = ");
+            do
+            {
+                if (temp != "")
+                {
+                    Console.WriteLine($"{varName} is not a valid number or value is out of MAX ({uint.MaxValue}) and MIN ({uint.MinValue}) borders");
+                    Console.Write($"{varName} = ");
+                }
+                temp = Console.ReadLine();
+
+            } while (!uint.TryParse(temp, out variable));
+            return variable;
+        }
+    }
+}
